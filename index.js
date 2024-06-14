9b6261933a8cdcd216b4c1b9d6f00dd0ba8c3e90
Praneeth <gudibandipraneeth@gmail.com>
2024-06-14 10:30:09 +0530

    MY NAME

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = '<h1>THIS IS PRANEETH REDDY( my first ci/cd pipeline)\n</h1>'
+  const msg = '<h1>THIS IS PRANEETH REDDY( my first ci/cd )\n</h1>'
   res.end(msg);
 });
 
